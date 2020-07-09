2700f5c238c002556aec620540b6497fc51f8a3d
guy fleury iteriteka <gfleury@disroot.org>
2020-07-09 23:36:45 +0200

    ADD input and print in contact.php

--- contact.php
+++ contact.php
@@ -1,7 +1,19 @@
 <?php include 'head.php'; ?>
 
-<div class="left-section">
-      HELLO    
-</div>
+<div class="right-section">
+      <form action="contact.php" method="post">
+          <label for="pseudo">Pseudo</label>
+          <input type="text" name="pseudo"><br>
+          <label for="email">Email </label>                       
+          <input type="text" name="email"><br>
+          <textarea name="txt"> </textarea><br>
+                 
+          <input type="submit" value="Print">
+      </form>
 
+            <?php
+            echo $_POST['pseudo'] . '<br>';
+            echo $_POST['email'] . '<br>';
+            echo $_POST['txt'] . '<br>';
+            ?>
 <?php include 'footer.php'; ?>
